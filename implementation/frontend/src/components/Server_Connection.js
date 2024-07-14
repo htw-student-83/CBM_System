8674958c16e225f47304a56d5a0878ab5121f2a0
Daniel <student2683@web.de>
2024-07-14 14:58:49 +0200

    created the under sites to select one of two servers, to start the lokal server and to start the tcp server

--- implementation/frontend/src/components/Server_Connection.js
+++ implementation/frontend/src/components/Server_Connection.js
@@ -1,14 +1,39 @@
+import {useEffect, useState} from "react";
+import { useNavigate } from 'react-router-dom'
+
 const Server_Connection = () => {
+    const [windowForLokalServer, setWindowForLokalServer] = useState(false);
+    const navigate = useNavigate();
+
+    const goToTCPServer = () =>{
+        navigate('/cashbox/serverArt/tcp');
+    }
+
+    const goToLocalServer = () =>{
+        navigate('/cashbox/serverArt/lokal');
+    }
+
     return(
         <div className="bg-sky-400 h-dvh w-auto">
-            <div className="mx-4 border border-amber-800 ml-auto mr-auto rounded-2xl p-4 w-fit">
-                <h1 className="text-2xl text-center">Welcher Server soll benutzt werden?</h1>
-                <div className="flex flex-grow">
-                    <button className="bg-white text-2xl rounded-2xl p-4 mx-24 my-4 hover:bg-emerald-200 border border-b-emerald-200">TCP</button>
-                    <button className="bg-amber-600 text-2xl rounded-2xl p-4 mx-28 my-4">Lokal</button>
+            <div className="py-60">
+                <div className="bg-neutral-100 mx-4 border-amber-800 ml-auto mr-auto rounded-2xl p-4 w-fit pl-6 pr-6">
+                    <h1 className="text-2xl text-center">Welcher Server soll für das Kassensystem verwendet werden?</h1>
+                    <div className="flex flex-grow mx-20">
+                        <button id='tcp' className="bg-white text-lg rounded-2xl p-2 mx-10 my-6 h-14 w-2/6 hover:bg-emerald-200 border-b-emerald-200"
+                            onClick={() => {
+                                goToTCPServer();
+                            }}
+                        >TCP</button>
+                         <button className="bg-white text-lg rounded-2xl p-2 mx-10 my-6 h-14 w-2/6  hover:bg-amber-200 border-amber-600"
+                             onClick={() => {
+                                 goToLocalServer();
+                             }}
+                        >Lokal</button>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
