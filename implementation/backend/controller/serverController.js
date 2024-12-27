b7c9430a1f1fae9a0e84e3814b8899402baeebef
Daniel <student2683@web.de>
2024-12-27 14:27:44 +0100

    implemented a possibility to connect a server by tcp

--- implementation/backend/controller/serverController.js
+++ implementation/backend/controller/serverController.js
@@ -1,58 +1,43 @@
+import express from 'express';
+import net from 'net';
+
+const app = express();
+const HOST = '192.168.178.23';
+const PORT = 4000;
+
 /**
  *
  * @param req
  * @param res
  * @returns {Promise<*>}
  */
-
-
 const checkLocalServer = async (req, res) => {
     return res.sendStatus(200);
 }
 
-/*
-const checkTCPConnection = async (req, res) => {
-    const socket = net.createConnection({ port, host}, () => {
-        res.sendStatus(200);
-        socket.end();
-    });
 
-    socket.on('error', (err) => {
-        res.sendStatus(500);
-        socket.destroy(); // Fehlerbehandlung
-    });
-};
-
+/**
+ *
+ * @param req
+ * @param res
  */
-
-import net from 'net';
-
-const TCP_HOST = '192.168.178.23';  // IP-Adresse des Servers
-const TCP_PORT = 4000;              // TCP-Port für den Server
-
-// Funktion zum Starten des TCP-Servers
-const startTCPServer = () => {
-    const server = net.createServer((socket) => {
-        console.log('Neue TCP-Verbindung');
-        socket.write('Verbindung erfolgreich!\n');
-        socket.on('data', (data) => {
-            console.log('Nachricht vom Client:', data.toString());
-        });
-        socket.on('end', () => {
-            console.log('Verbindung geschlossen');
-        });
+const startTCPServer = (req, res) => {
+    // Versuchen, eine Verbindung zum TCP-Server herzustellen
+    const socket = net.createConnection({ port: PORT, host: HOST }, () => {
+        res.status(200).send('TCP-Verbindung erfolgreich!');
+        socket.end();  // Verbindung schließen
     });
 
-    server.listen(TCP_PORT, TCP_HOST, () => {
-        console.log(`TCP-Server läuft auf ${TCP_HOST}:${TCP_PORT}`);
-    });
-
-    server.on('error', (err) => {
-        console.error('Fehler im TCP-Server:', err.message);
+    // Wenn ein Fehler auftritt, sende Status 500 (Fehler) zurück
+    socket.on('error', (err) => {
+        console.error('Fehler bei der TCP-Verbindung:', err.message);
+        res.status(500).send('Fehler bei der TCP-Verbindung');
     });
 };
 
-// Server starten
+app.listen(PORT, () => {
+    console.log(`HTTP-Server läuft auf http://localhost:${PORT}`);
+});
 
 
 export default {
