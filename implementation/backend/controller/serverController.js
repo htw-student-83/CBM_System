b5aac5dfeb75f489c246130ab054be16f5b1740b
Daniel <student2683@web.de>
2024-12-27 13:56:46 +0100

    next update for the new version of tcp server connection

--- implementation/backend/controller/serverController.js
+++ implementation/backend/controller/serverController.js
@@ -1,20 +1,16 @@
-import net from "net";
-
-const host = '192.168.178.23';
-const port = 4000;// Ersetze dies mit der Adresse des Servers // Ersetze dies mit dem Port, den du überprüfen möchtest
-
-
 /**
  *
  * @param req
  * @param res
  * @returns {Promise<*>}
  */
+
+
 const checkLocalServer = async (req, res) => {
     return res.sendStatus(200);
 }
 
-
+/*
 const checkTCPConnection = async (req, res) => {
     const socket = net.createConnection({ port, host}, () => {
         res.sendStatus(200);
@@ -27,7 +23,39 @@ const checkTCPConnection = async (req, res) => {
     });
 };
 
+ */
+
+import net from 'net';
+
+const TCP_HOST = '192.168.178.23';  // IP-Adresse des Servers
+const TCP_PORT = 4000;              // TCP-Port für den Server
+
+// Funktion zum Starten des TCP-Servers
+const startTCPServer = () => {
+    const server = net.createServer((socket) => {
+        console.log('Neue TCP-Verbindung');
+        socket.write('Verbindung erfolgreich!\n');
+        socket.on('data', (data) => {
+            console.log('Nachricht vom Client:', data.toString());
+        });
+        socket.on('end', () => {
+            console.log('Verbindung geschlossen');
+        });
+    });
+
+    server.listen(TCP_PORT, TCP_HOST, () => {
+        console.log(`TCP-Server läuft auf ${TCP_HOST}:${TCP_PORT}`);
+    });
+
+    server.on('error', (err) => {
+        console.error('Fehler im TCP-Server:', err.message);
+    });
+};
+
+// Server starten
+
+
 export default {
     checkLocalServer,
-    checkTCPConnection,
+    startTCPServer
 }
--- implementation/backend/routes/serverRoutes.js
+++ implementation/backend/routes/serverRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
 import serverRoutes from "../controller/serverController.js";
-const { checkTCPConnection, checkLocalServer } = serverRoutes;
+const { startTCPServer, checkLocalServer } = serverRoutes;
 
 //Route to check the status of the lokal server
 router.get('/localserver/status', checkLocalServer);
 
 //Route to check the status of the tcp server
-router.get('/tcpserver/status', checkTCPConnection);
+router.get('/tcpserver/status', startTCPServer);
 
 export default router
