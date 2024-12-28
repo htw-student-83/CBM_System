8af47b441edfede9f3145ace38f98d68ed33232f
Daniel <student2683@web.de>
2024-12-28 21:09:14 +0100

    changed the style and positions of the components tcp connection, server connection and lokal connection

--- implementation/frontend/src/components/Server_Connection.js
+++ implementation/frontend/src/components/Server_Connection.js
@@ -3,24 +3,21 @@ import { useNavigate } from 'react-router-dom'
 const Server_Connection = () => {
     const ip_address = "192.168.178.23"
     const local_address = "localhost"
-
     const navigate = useNavigate();
 
     const goToTCPServer = () =>{
-        console.log("Übertragene IP: " + ip_address)
         navigate('/cashbox/serverArt/tcp', { state: { message: ip_address }});
     }
 
     const goToLocalServer = () =>{
-        console.log("Übertragene IP: " + local_address)
         navigate('/cashbox/serverArt/lokal', { state: { message: local_address }});
     }
 
     return(
         <div className="bg-sky-400 h-dvh w-auto">
-            <div className="py-60">
+            <div className="py-72">
                 <div className="bg-neutral-100 mx-4 border-amber-800 ml-auto mr-auto rounded-2xl p-4 w-fit pl-6 pr-6">
-                    <h1 className="text-2xl text-center">Soll der locale Server für das Kassensystem verwendet werden?</h1>
+                    <h1 className="text-xl text-center px-10">Soll der locale Server für das Kassensystem verwendet werden?</h1>
                     <div className="flex flex-grow mx-20">
                         <button id='tcp' className="bg-white text-lg rounded-2xl p-2 mx-10 my-6 h-14 w-2/6 hover:bg-emerald-200 border-b-emerald-200"
                             onClick={() => {
--- implementation/frontend/src/components/TCP_Connection.js
+++ implementation/frontend/src/components/TCP_Connection.js
@@ -35,15 +35,15 @@ const TCP_Connection = () => {
 
     return(
         <div className="bg-sky-400 h-dvh w-auto">
-            <div className="py-80">
-                <div className="bg-neutral-100 mx-4 border-amber-800 ml-auto mr-auto rounded-2xl py-10 w-fit pl-10 pr-10">
-                    <h1 className="text-xl text-center">Starten Sie bitte den Server auf dem anderen Rechner...</h1>
-                    <div className="flex flex-grow mx-20">
-                        <button id='ok' className="bg-white text-lg rounded-2xl p-2 mx-10 my-6 h-14 w-2/6 hover:bg-emerald-200 border-b-emerald-200"
+            <div className="py-72">
+                <div className="bg-neutral-100 mx-4 border-amber-800 ml-auto mr-auto rounded-2xl py-5 w-fit pl-10 pr-10">
+                    <h1 className="text-xl text-center py-3">Starten Sie bitte den Server auf dem anderen Rechner.</h1>
+                    <div className="flex flex-grow px-2">
+                        <button id='ok' className="bg-white w-52 text-lg rounded-2xl p-2 mx-10 my-4 h-14 hover:bg-emerald-200 border-b-emerald-200"
                             onClick={handleRemoteServer}
                         >ok
                         </button>
-                        <button className="bg-white text-lg rounded-2xl p-2 mx-10 my-6 h-14 w-3/5 hover:bg-neutral-200 border-b-emerald-200"
+                        <button className="bg-white w-52 text-lg rounded-2xl p-2 mx-10 my-4 h-14 hover:bg-neutral-200 border-b-emerald-200"
                             onClick={() => {
                               goToServer_Connection();
                             }}
